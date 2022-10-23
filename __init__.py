b7e6ef0d12a0afbb1a983fb488534ae0155aeadf
ds1219 <76910979+ds1219@users.noreply.github.com>
2022-10-23 22:18:58 +0530

    added filters
    added filters
    graph and table update to match filter

--- __init__.py
+++ __init__.py
@@ -10,14 +10,14 @@ from os.path import getmtime
 from os.path import basename
 
 # production
-inputFolder = '/mnt/volume_annif_projects/data-sets/bldg-regs/docs/validate/nn-bv-stw-ensemble-en/*.json'
-notesFile = '/mnt/volume_annif_projects/data-sets/bldg-regs/docs/validate/nn-bv-stw-ensemble-en/MachineLearning.md'
+inputFolder = "/mnt/volume_annif_projects/data-sets/bldg-regs/docs/validate/nn-bv-stw-ensemble-en/*.json"
+notesFile = "/mnt/volume_annif_projects/data-sets/bldg-regs/docs/validate/nn-bv-stw-ensemble-en/MachineLearning.md"
 
 # testing
-#inputFolder = 'json/*.json'
-#notesFile = 'Machine_Learning.md'
+# inputFolder = "data-sets/*.json"
+# notesFile = "data-sets/MachineLearning.md"
 
-seconds = 5  # change to 60 for a minute
+seconds = 30  # change to 60 for a minute
 
 
 def cleanTitles():
@@ -26,14 +26,14 @@ def cleanTitles():
     jsonData = [basename(x) for x in jsonData]
     jsonData = [x[0:-5] for x in jsonData]
 
-    return (jsonData)
+    return jsonData
 
 
 def getjson():
     # get json files and sort by creation date
     jsonData = glob(inputFolder)
     jsonData.sort(key=getmtime)
-    return (jsonData)
+    return jsonData
 
 
 def parsejson():
@@ -41,115 +41,192 @@ def parsejson():
 
     count = 1  # count is used to configure the number of ticks on the xaxis
 
-    template = {"Index": [],
-                "Precision_doc_avg": [],
-                'Recall_doc_avg': [],
-                'F1_score_doc_avg': []}
+    template = {
+        "Index": [],
+        "Precision_doc_avg": [],
+        "Recall_doc_avg": [],
+        "F1_score_doc_avg": [],
+    }
 
     jsonData = getjson()
 
     # read json files
     for file in jsonData:
-        with open(file, 'r') as f:
+        with open(file, "r") as f:
             data = json.load(f)
-            data['Index'] = count
+            data["Index"] = count
 
             for k in template.keys():
                 template[k].append(data[k])
 
         count += 1
+
+    template["Title"] = cleanTitles()
     df = pd.DataFrame(template)
-    return (df)
+    return df
 
 
 def parseNotes():
 
-    template = {'titles': [],
-                'ml model': [],
-                'date': [],
-                'sources': [],
-                'vocab': [],
-                'training': [],
-                'incremental learning': [],
-                'comments': []}
+    template = {
+        "titles": [],
+        "ml model": [],
+        "date": [],
+        "sources": [],
+        "vocab": [],
+        "training": [],
+        "incremental learning": [],
+        "comments": [],
+    }
 
-    with open(notesFile, 'r') as f:
+    with open(notesFile, "r") as f:
         data = f.readlines()
 
-    data = [x.replace('\n', '') for x in data]
-    titles = [x.replace('## ', '') for x in data if '##' in x]
-    text = [data[data.index(x) + 1: data.index(x) + 1 + 7]
-            for x in data if '##' in x]
+    data = [x.replace("\n", "") for x in data]
+    titles = [x.replace("## ", "") for x in data if "##" in x]
+    text = [data[data.index(x) + 1 : data.index(x) + 1 + 7] for x in data if "##" in x]
 
     for note in text:
         for j in note:
-            j = j.split('=')
+            j = j.split("=")
             j = [x.strip() for x in j]
 
             template[j[0].lower()].append(j[1])
 
-    template['titles'] = titles
+    template["titles"] = titles
 
     df = pd.DataFrame(template)
-    return (df)
+    return df
 
 
 app = dash.Dash(__name__, external_stylesheets=[dbc.themes.BOOTSTRAP])
 app.layout = html.Div(
     [
         html.H2(
-            'Building Regulation Guidance (Machine Learning (ML) Natural language processing (NLP) automated subject indexing'),
-        dcc.Graph(id='ml', animate=True),  # ml graph
-        html.Div(id='table'),  # notes table
-        dcc.Interval(id='graph-update', interval=seconds*1000),
+            "Building Regulation Guidance (Machine Learning (ML) Natural language processing (NLP) automated subject indexing"
+        ),
+        dcc.Dropdown(id="dropdown_model", multi=True),
+        dcc.Dropdown(id="dropdown_sources", multi=True),
+        dcc.Dropdown(id="dropdown_vocab", multi=True),
+        dcc.Dropdown(id="dropdown_training", multi=True),
+        html.H4(id="graphError"),
+        dcc.Graph(id="ml", animate=True),  # ml graph
+        html.Div(id="table"),  # notes table
+        dcc.Interval(id="update", interval=seconds * 1000),
     ]
 )
 
 
 # updater for ML Graph
 
-@ app.callback(Output('ml', 'figure'),
-               [Input('graph-update', 'n_intervals')])
-def update_graph_scatter(input_data):
+
+@app.callback(
+    [
+        Output("ml", "figure"),
+        Output("table", "children"),
+        Output("dropdown_model", "options"),
+        Output("dropdown_sources", "options"),
+        Output("dropdown_vocab", "options"),
+        Output("dropdown_training", "options"),
+        Output("graphError", "children"),
+    ],
+    [
+        Input("update", "n_intervals"),
+        Input("dropdown_model", "value"),
+        Input("dropdown_sources", "value"),
+        Input("dropdown_vocab", "value"),
+        Input("dropdown_training", "value"),
+    ],
+)
+def updateAll(n_intervals, ddv_models, ddv_sources, ddv_vocab, ddv_training):
 
     df = parsejson()
 
-    fig = go.Figure()
+    notes = parseNotes()
+    query = []
 
+    # dropdowns
+    ddModels = notes["ml model"].unique()
+    ddSources = notes["sources"].unique()
+    ddVocab = notes["vocab"].unique()
+    ddTraining = notes["training"].unique()
+
+    if not (ddv_models == None or ddv_models == []):
+        query.append("`ml model` == @ddv_models")
+
+    if not (ddv_sources == None or ddv_sources == []):
+        query.append("sources == @ddv_sources")
+
+    if not (ddv_vocab == None or ddv_vocab == []):
+        query.append("vocab == @ddv_vocab")
+
+    if not (ddv_training == None or ddv_training == []):
+        query.append("training == @ddv_training")
+
+    if query != []:
+        query = " and ".join(query)
+        notes = notes.query(query)
+        df = df.query('Title == @notes["titles"].values.tolist()')
+    else:
+        notes = notes
+
+    # graph
+    fig = go.Figure()
     # pres_doc_avg line
-    fig.add_trace(go.Scatter(
-        x=df['Index'], y=df['Precision_doc_avg'], mode='lines', name='Precision_doc_avg', line_shape='spline'))
+    fig.add_trace(
+        go.Scatter(
+            x=df["Index"],
+            y=df["Precision_doc_avg"],
+            mode="lines",
+            name="Precision_doc_avg",
+            line_shape="spline",
+        )
+    )
 
     # recall_doc_avg line
-    fig.add_trace(go.Scatter(
-        x=df['Index'], y=df['Recall_doc_avg'], mode='lines', name='Recall_doc_avg', line_shape='spline'))
+    fig.add_trace(
+        go.Scatter(
+            x=df["Index"],
+            y=df["Recall_doc_avg"],
+            mode="lines",
+            name="Recall_doc_avg",
+            line_shape="spline",
+        )
+    )
 
     # f1_score_doc_avg line
-    fig.add_trace(go.Scatter(
-        x=df['Index'], y=df['F1_score_doc_avg'], mode='lines', name='F1_score_doc_avg', line_shape='spline'))
+    fig.add_trace(
+        go.Scatter(
+            x=df["Index"],
+            y=df["F1_score_doc_avg"],
+            mode="lines",
+            name="F1_score_doc_avg",
+            line_shape="spline",
+        )
+    )
 
     fig.update_layout(
         xaxis=dict(
             # set custom x axis ticks and ticklabels
-            tickmode='array',
-            tickvals=list(df['Index']),
-            ticktext=cleanTitles(),))
+            tickmode="array",
+            tickvals=df["Index"],
+            ticktext=df["Title"],
+        )
+    )
 
-    return (fig)
+    # Error
+    if len(df) == 1:
+        errorMSG = "! Query contains only one record. Cannot Plot"
+    else:
+        errorMSG = ""
 
+    # table
+    tbl = dbc.Table.from_dataframe(notes, bordered=True, responsive=True, striped=True)
 
-# updater for Notes Table
-
-@ app.callback(Output('table', 'children'),
-               [Input('graph-update', 'n_intervals')])
-def search_fi(n_intervals):
-    df = parseNotes()
-    tbl = dbc.Table.from_dataframe(
-        df, bordered=True, responsive=True, striped=True)
-    return (tbl)
+    return (fig, tbl, ddModels, ddSources, ddVocab, ddTraining, errorMSG)
 
 
 server = app.server
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     app.run_server(debug=False)
